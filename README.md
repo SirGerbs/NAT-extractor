cc3dd2147c841a4d110eceded1f27de22801949b
SirGerbs <gerben_kleijn@hotmail.com>
2016-04-14 13:14:30 -0700

    mask_to_cidr
    Added functionality to print subnets in CIDR notation. Also cleaned up
    the code and added comments.

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 Nat-Extractor
 
-usage: python extr-nat.py [options] config_file
+Summary: Parse out named objects and NAT tables from a Cisco firewall configuration file. 
 
 Dependencies: argparse
 
-Summary: Parse out named objects and NAT tables from a Cisco firewall configuration file. 
+usage: extr_nat.py [-h] [-i] [-o] [-n] [-c] config_file
+
+positional arguments:
+  config_file   Cisco Firewall Configuration File
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -i, --ip      convert all named objects to IP addresses
+  -o, --object  parses out named objects
+  -n, --nat     parses out NAT tables
+  -c, --cidr    display subnets in CIDR notation
 
 TODO:
 1. clean up code
--- extr_nat.py
+++ extr_nat.py
@@ -1,7 +1,6 @@
 import io
 import re
 import sys
-#import netaddr
 import argparse
 
 #class to hold global variables and such
@@ -9,25 +8,14 @@ class mem:
 	ip = False
 	object = False
 	nat = False
+	cidr = False
 	file_dict = {}
 	ip_dict = {}
 	nat_dict = {}
 	port_dict = {}
 
-#verify user entered enough cmd line arguments
-def check_args():
-	#print "in check_args"
-	
-	if len(sys.argv) < 2:
-		print "Usage: python extr_nat.py configuration_file"
-		exit(1)
-	
-	return 0
-	
 #prepopulate mem.port_dict with known port aliases
 def prepop_dict():
-	#print "in prepop_dict"
-	
 	mem.port_dict["ah"] = "51"
 	mem.port_dict["eigrp"] = "88"
 	mem.port_dict["esp"] = "50"
@@ -122,9 +110,8 @@ def prepop_dict():
 	
 	return 0
 	
-#function meant to store config file lines in dict, so that no "intermediate" file needs to be used
+#Modify input config file to remove unwanted lines, and then store in file_dict for later usage
 def file_to_dict(config_file):
-	#print "in file_to_dict"
 	line_counter = 0
 	with open(config_file, "r") as file:
 		for line in file:
@@ -146,31 +133,21 @@ def file_to_dict(config_file):
 	
 	return 0
 
-#primitive netmask to CIDR conversion. takes result from re.match as argument
-#def mask_to_cidr2(netmask):
-#	#print "in mask_to_cidr2"
-#	
-#	#obtain the part of netmask we're interested in and remove unwanted whitespace at the beginning of the line
-#	mask = netmask.group(0)
-#	mask = re.sub("^\s", "", mask)
-#	
-#	#if the string is not in the format of a netmask
-#	if not re.match(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", mask):
-#		print mask + " is not a netmask"
-#		exit(1)
-#	
-#	#if string is a netmask find the four octets and convert them to binary
-#	else:
-#		octets = re.findall(r"\d{1,3}", mask)
-#		binary = format(int(octets[0]), "b") + format(int(octets[1]), "b") + format(int(octets[2]), "b") + format(int(octets[3]), "b")
-#	
-#	#return the number of 1's that appear in the binary form of the octets
-#	return "/" + str(binary.count("1"))
+#primitive netmask to CIDR conversion.
+def mask_to_cidr(netmask):
+	netmask = re.search("^(?P<ip>(\d{1,3}\.){3}\d{1,3}) (?P<mask>(\d{1,3}\.){3}\d{1,3})$", netmask)
+	ip = netmask.group("ip")
+	mask = netmask.group("mask")
+	
+	#if string is a netmask find the four octets and convert them to binary
+	octets = re.findall(r"\d{1,3}", mask)
+	binary = format(int(octets[0]), "b") + format(int(octets[1]), "b") + format(int(octets[2]), "b") + format(int(octets[3]), "b")
+	
+	#return the number of 1's that appear in the binary form of the octets
+	return ip + "/" + str(binary.count("1"))
 
 #Extract network objects into mem.ip_dict
 def extract_network_objects():
-	#print "in extract_network_objects"
-	
 	#object variable to store named objects
 	object = ""
 	
@@ -186,62 +163,54 @@ def extract_network_objects():
 		if re.match(r".*range \d+ \d+$", line):
 			line = re.sub(r"(?P<one>range \d+) (?P<two>[^\s]+)$", r"\g<one>-\g<two>", line)
 		
-		#if the line declares a new object
+		#if the line declares a new object, store the object name in variable so it can be used later; during subsequent line iterations
 		if re.match("\s?object (network |service ).*", line):
-			#print line
 			object_group = re.search(r"(^\s?object (network |service ))(?P<this>[^\s]+)$", line)
 			object = object_group.group("this")
 			
 		#regex matches "host <IP>"
-		elif re.match(r'\s?host \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line):
+		elif re.match(r'\s?host (\d{1,3}\.){3}\d{1,3}', line):
 			#extract IP address belonging to object				
-			ip_group = re.search(r'(\s?host )(?P<this>[^\s]+)$', line)
-			ip = ip_group.group("this")
-			
+			ip = re.search(r'(\s?host )(?P<this>[^\s]+)$', line)
+						
 			#add network object and ip to dict
-			mem.ip_dict[object] = ip
-			#print "1" + object + ip
+			mem.ip_dict[object] = ip.group("this")
 		
 		#regex matches "range <IP>-<IP>"
-		elif re.match(r'\s?range \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}-\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line):
+		elif re.match(r'\s?range (\d{1,3}\.){3}\d{1,3}-(\d{1,3}\.){3}\d{1,3}', line):
 			range = re.search(r".*range (?P<this>[^\s]+)$", line)
 			
 			#add network object and ip to dict
 			mem.ip_dict[object] = range.group("this")
-			#print "2" + object + range.group("this")
+			
 		#line declares a subnet
-		elif re.match(r"\s?subnet\s\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line):
-			#print "i found a subnet"
-			subnet = re.search(r"(\s?subnet )(?P<this>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$", line)
+		elif re.match(r"\s?subnet\s(\d{1,3}\.){3}\d{1,3}\s(\d{1,3}\.){3}\d{1,3}", line):
+			subnet = re.search(r"(\s?subnet )(?P<this>(\d{1,3}\.){3}\d{1,3}\s(\d{1,3}\.){3}\d{1,3})$", line)
 			
 			#add network object and ip to dict
 			mem.ip_dict[object] = subnet.group("this")
-			#print "3" + object + subnet.group("this")
+			
 		#line declares a service
 		elif re.match(r"\s?service (tcp |udp )", line):
-			service_group = re.search(r"(.* )(?P<this>[^\s]+)$", line)
-			service = service_group.group("this")
+			service = re.search(r"(.* )(?P<this>[^\s]+)$", line)
 			
-			mem.ip_dict[object] = service
-			#print "4" + object + service
+			#add network object and ip to dict			
+			mem.ip_dict[object] = service.group("this")
+			
 		iter_counter += 1
-	#for key, value in mem.ip_dict.iteritems():
-	#	print key, value
+		
 	return 0
 	
 #extract object-group networks into mem.ip_dict
 def extract_object_group_networks():
-	#print "in extract_object_group_networks"
-	
 	#list and variable to hold object and lines that belong to that object
 	lines = []
 	object_group = ""
 	
-	#go through the mem.file_dict one line at a time
 	iter_counter = 0
 	while iter_counter < len(mem.file_dict):
 	
-	#go through output file one line at a time
+	#go through the mem.file_dict one line at a time
 		line = mem.file_dict[iter_counter]
 		
 		#take out newline at the end
@@ -251,9 +220,9 @@ def extract_object_group_networks():
 		if re.match(r".*range \d+ \d+$", line):
 			line = re.sub(r"(?P<one>range \d+) (?P<two>[^\s]+)$", r"\g<one>-\g<two>", line)
 			
-		#if the line declares a new object-group
+		#if the line declares a new object-group, store it in the object_group variable for later use - during a later line iteration
 		if re.match(r'^object-group.*', line):
-			#if it's not the first time an object-group is declared, add hosts from previous lines to the dict, then reset
+			#If the line declares a new object, add previous object plus IPs to the dict, then reset. This does not fire if it's the first time an object is declared
 			if object_group != "":
 				mem.ip_dict[object_group] = lines
 			lines = []
@@ -300,9 +269,8 @@ def extract_object_group_networks():
 		
 	return 0
 	
-#return IP for object
+#return IP address or port number for named object
 def dict_lookup(str):
-	#print "in dict_lookup"
 	if str in mem.ip_dict:
 		return mem.ip_dict[str]
 	elif str in mem.port_dict:
@@ -310,9 +278,8 @@ def dict_lookup(str):
 	else:
 		return 0
 
-#replace named objects with IP addresses
+#replace named objects with IP addresses. This function has some recursion because a named object could contain a list of named objects, which in turn could also contain lists of named objects
 def name_to_ip(dict):
-	#print "in name_to_ip"
 	for key, value in dict.iteritems():
 		#the value can be a single IP address, or a list.
 		if isinstance(value, list):
@@ -321,7 +288,7 @@ def name_to_ip(dict):
 				position = value.index(i)
 				counter = position
 				
-				#if the current list item also appears in mem.ip_dict as a key, meaning it must be a named object
+				#if the current list item also appears in mem.ip_dict as a key it must be a named object
 				if i in mem.ip_dict or i in mem.port_dict:
 					#The value for the named object (i) could be another list
 					if isinstance(dict_lookup(i), list):
@@ -341,12 +308,14 @@ def name_to_ip(dict):
 					pass
 		
 		#if the value is not a list then it could be an IP address or port and nothing else needs to be done
-		elif re.match(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\d)", value):
+		elif re.match(r"((\d{1,3}\.){3}\d{1,3}|\d+)", value):
 			pass
 		
+		#if the value is not a list, and not an IP address or port, then it must be a named object
 		else:
 			name_to_ip = dict_lookup(value)
 			
+			#again, named objects could contain lists so we have to 'unpack' them down to individual IP addresses
 			if isinstance(name_to_ip, list):
 				for i in name_to_ip:
 					#store the index value of the current list item, and duplicate as counter
@@ -370,92 +339,86 @@ def name_to_ip(dict):
 							
 					#if the current list item does not appear in mem.ip_dict as a key, then it should itself be an IP address and nothing needs to be done. 
 					else:
-						#print i
 						pass
+			
+			#if the named object does not contain a list of values, do nothing. It seems to me that a named object could contain another named object, so at some point if a named object does not get translated to an IP address, this is where the problem could be
 			else:
 				pass
 			
+			#replace the dict entry with the new dict entry. I read it's a bad habit to modify a dict while iterating through it, so this may need to be changed in the future
 			del dict[key]
 			dict[key] = name_to_ip
 				
-			
-	#for key, value in dict.iteritems():
-	#	print key, value
 	return dict
 
-#populate the mem.nat_dict with info from the configuration file
+#populate the mem.nat_dict with NAT table info from the configuration file
 def pop_nat_dict():
-	#print "in parse_nat"
-	#go through the mem.file_dict one line at a time
+	#declare variables to be used in this function
 	iter_counter = 0
 	trigger = 0
 	nat_inside = ""
 	nat_outside = ""
+	
 	while iter_counter < len(mem.file_dict):
-	#with open(sys.argv[2], "r") as nat_file:
+		#go through the mem.file_dict one line at a time
 		line = mem.file_dict[iter_counter]
-		#for line in nat_file:
+		
+		#take out possible whitespace at the end of the line
 		line = re.sub(r"\s$","",line)
-				
+		
+		#this matches "nat (inside,outside) after-auto source dynamic <object> <object>
 		if re.match(r"\s?nat \([^\s]+,[^\s]+\)( after-auto)? source.*", line):
 			inside_group = re.search(r".*(dynamic |static )(?P<this>[^\s]+)", line)
 			outside_group = re.search(r".*(dynamic | static )([^\s]+\s)(?P<this>[^\s]+)", line)
-			#print "!!!!!!!!!!!!!!!!!!!!!!!!!!"
 			inside = inside_group.group("this")
 			outside = outside_group.group("this")
-			#print inside + " -- " + outside
 			
 			mem.nat_dict[outside] = inside
 		
+		#this matches if the line declares an object network. Since this is the same syntax for NAT as it is for the declaration of network objects earlier in the config file, we're using a trigger variable to distinguish between the two
 		elif re.match(r"\s?object network.*", line):
-			#print line
 			trigger = 0
 			nat_object_group = re.search(r"(.*) (?P<this>[^\s]+)$", line)
 			nat_object = nat_object_group.group("this")
 			
+			#the nat object should appear in the mem.ip_dict, as it should have been defined previously. If this is the case, the trigger variable is set to 1
 			if nat_object in mem.ip_dict:
 				trigger = 1
-				#print "!!!!!!"
-				#print nat_object
 				nat_inside = nat_object
-						
+		
+		#If the line declares a NAT translation that should be associated with the network object that came in the line before it
 		elif re.match(r"^\s?nat .*", line) and trigger == 1:
-			#print "!!!!!!!"
 			nat_outside_group = re.search(r"(.*) (?P<this>[^\s]+)$", line)
 			nat_outside = nat_outside_group.group("this")
 			
 			mem.nat_dict[nat_outside] = nat_inside
-			#print nat_outside + "!!!!!!"
-		#if nat_inside != "" and nat_outside != "":
-			#print nat_outside + " -- " + nat_inside
 		
 		iter_counter += 1
 	
 	return 0
 
+#this function exists to translate dict keys that are named objects to IP addresses
 def key_to_ip():
-	
 	new_key_values = {}
+	#iterate through mem.nat_dict and check to see if the key is define in mem.ip_dict. If so, place IP in new_key_values dict. This does not fire if the key is a named object that declares a list, as the key for a dict cannot be a list
 	for key, value in mem.nat_dict.iteritems():
 		if key in mem.ip_dict and not isinstance(dict_lookup(key), list):
 			new_key = dict_lookup(key)
 			new_key_values[new_key] = value
-			#print "1 " + new_key + ", " + value
+			
+			#change value of original mem.nat_dict key, so it can be found later for deletion
 			mem.nat_dict[key] = "-1"
-			#print "2 " + key + ", " + value
+	
+	#iterate through the new_key_values dict and use the entries to replace entries in the original mem.nat_dict
 	for key, value in new_key_values.iteritems():
 		mem.nat_dict[key] = value
 	
+	#delete all entries from the dict where value == "-1"
 	mem.nat_dict = {k:v for k,v in mem.nat_dict.items() if v != '-1'}
-	#mem.nat_dict = new_dict
-	#for key, value in new_key_values.iteritems():
-	#	print key, value
 	
 	return 0
 
-#replace 
-	
-#print out the mem.file_dict
+#print out the mem.file_dict. This function is not used in the program and only exists for troubleshooting purposes
 def print_file_dict():
 	iter_counter = 0
 	while iter_counter < len(mem.file_dict):
@@ -464,13 +427,33 @@ def print_file_dict():
 	
 	return 0
 
-#print out the mem.nat_dict
+#print out a dict
 def print_dict(dict):
 	for key, value in dict.iteritems():
-		print key, value
+	
+		#if the user specified that they want to receive subnets in CIDR notation, this part of the function executes to replace netmasks with CIDR entries
+		if mem.cidr == True:
+			if isinstance(value, list): 
+				cidr_list = []
+				for i in value:
+					if re.match(r"(\d{1,3}\.){3}\d (\d{1,3}\.){3}\d", i):
+						cidr_list.append(mask_to_cidr(i))
+					else:
+						cidr_list.append(i)
+				print key, str(cidr_list)
+			elif re.match(r"(\d{1,3}\.){3}\d (\d{1,3}\.){3}\d", value):
+				cidr_value = mask_to_cidr(value)
+				print key, cidr_value
+			else:
+				print key, value	
+		
+		#if the user did not specify CIDR notation, just print key and value
+		else:
+			print key, value
 	
 	return 0
-	
+
+#main program function
 def main():
 	#argument parsing, later to be put into a function
 	parser = argparse.ArgumentParser()
@@ -478,24 +461,31 @@ def main():
 	parser.add_argument("-i", "--ip", help="convert all named objects to IP addresses", action="store_true")
 	parser.add_argument("-o", "--object", help="parses out named objects", action = "store_true")
 	parser.add_argument("-n", "--nat", help="parses out NAT tables", action = "store_true")
+	parser.add_argument("-c", "--cidr", help="display subnets in CIDR notation", action = "store_true")
 	args = parser.parse_args()
 	
+	#if the user did not execute the program correctly
 	if not args.object and not args.nat:
 		print "You have to specify at least -o or -n\n"
 		exit(1)
 	
+	#set the specified arguments as variables in the mem class
 	if args.ip:
 		mem.ip = True
 	if args.object:
 		mem.object = True
 	if args.nat:
 		mem.nat = True
+	if args.cidr:
+		mem.cidr = True
 	
+	#These functions always need to execute for the program to do its job
 	prepop_dict()	
 	file_to_dict(args.config_file)
 	extract_network_objects()
 	extract_object_group_networks()
 	
+	#depending on the cmd line arguments specified, execute specific functions
 	if args.object and not args.nat:
 		my_dict = mem.ip_dict
 	elif args.nat and not args.object:
